9cf42ed94ac5515d55d8fb7738d8d42687c2781e
nathannrss <natans751@gmail.com>
2025-03-26 16:48:10 -0300

    Ajuste Arquivo main.py
    ajuste de arquivo

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"massage": "Hello Word"} 
+
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": "deu certo"}
