889f35ce23da4a6ed96d7473f0065f23297cd794
nathannrss <natans751@gmail.com>
2025-04-16 17:28:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,21 @@ async def root():
 async def funcaoteste():
     return {"teste": True, "num-aleator": random.randint(0, 1000)}
 
-a
+
+
+
+#teste erro
+
+
+# test_main.py
+
+def test_erro_simulado():
+    assert False, "Erro simulado para testar falha no pipeline"
+
+    - name: Forçar erro
+  run: exit 1
+
+
+
+def erro_de_sintaxe(
+    print("erro")  # falta fechar parêntese da função
